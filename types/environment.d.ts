b20cb0ae05672d7a6ff32ff9f9202f1f3efc7a6d
Stanley Wong <s339428326@gmail.com>
2023-06-06 11:25:28 +0800

    docs:add type folder and .env.sample

---
+++ types/environment.d.ts
@@ -0,0 +1,23 @@
+// declare global {
+//   namespace NodeJS {
+//     interface ProcessEnv {
+//       // NODE_ENV: 'development' | 'production';
+//       PORT: string;
+//       DB_USERNAME: string;
+//       DB_PASSWORD: string;
+//       DATABASE: string;
+//       JWT_SECRET: string;
+//       JWT_EXPIRES_IN: string;
+//       COOKIE_EXPIRES_IN: string;
+//     }
+//   }
+//   // namespace Express {
+//   //   interface Middleware {
+//   //     res: Response;
+//   //     req: Request;
+//   //     next: NextFunction;
+//   //   }
+//   // }
+// }
+
+// export {};
